62cf4a06c16c9642d8aa1e5e7ce949a3adf42090
Adam Li <adam@bizzone.com>
2024-08-19 11:14:49 -0400

    setup nativewind

--- src/app/(tabs)/index.tsx
+++ src/app/(tabs)/index.tsx
@@ -1,6 +1,8 @@
-import {Text} from 'react-native';
+import { Text, View } from "react-native";
 export default function FeedScreen() {
-    return(
-        <Text className='text-3xl'>About us</Text>
-    )
+  return (
+    <View className="bg-slate-600 flex-1 items-center justify-center m-10 ">
+      <Text className="text-5xl font-bold text-blue-500">About us</Text>
+    </View>
+  );
 }
